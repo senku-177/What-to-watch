6ad73badddf9a9d4178665dbda9b1f7c5b24fabd
senku-177 <goyalsanyam20@gmail.com>
2024-04-05 22:09:16 +0530

    initial commit

---
+++ src/components/Description.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../CSS/Description.css";
+
+function Description(props) {
+	return (
+		<div className="popup-box">
+			<div className="box">
+			<button class="close-button" onClick={props.handleClose}></button>
+
+				<div className="d-flex flex-row ">
+					<div className="p-2 text">
+						Can't you just decide one movie to watch with your friends? We will
+						find movies for you if you each enter one of your favourite genres.
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Description;
